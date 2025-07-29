142ec65da15eda1f5310c8d8384c228990666b03
Aaren08 <schneiderneil392@gmail.com>
2025-07-29 08:30:19 +0500

    Created the ManageCars and ManageBookings page

--- frontend/src/pages/Owner/ManageBookings/ManageBookings.jsx
+++ frontend/src/pages/Owner/ManageBookings/ManageBookings.jsx
@@ -1,7 +1,97 @@
+import { useEffect, useState } from "react";
+import Title from "../../../components/Owner/Title/Title.jsx";
+import { dummyMyBookingsData } from "../../../assets/assets.js";
 import "./ManageBookings.css";
 
 const ManageBookings = () => {
-  return <div>ManageBookings</div>;
+  const [bookings, setBookings] = useState([]);
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const fetchOwnerBookings = async () => {
+    setBookings(dummyMyBookingsData);
+  };
+
+  useEffect(() => {
+    fetchOwnerBookings();
+  }, []);
+
+  return (
+    <div className="manage-bookings">
+      <Title
+        title={"Manage Bookings"}
+        subtitle={
+          "Track all customer bookings, approve or cancel requests, and manage booking statuses."
+        }
+      />
+
+      {/* TABLE */}
+
+      <div className="manage-bookings-table-container">
+        <table className="manage-bookings-table">
+          <thead>
+            <tr className="manage-bookings-table-header">
+              <th>Car</th>
+              <th>Date Range</th>
+              <th>Total</th>
+              <th>Payment</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {bookings.map((booking, index) => (
+              <tr key={index} className="manage-bookings-table-row">
+                <td className="manage-bookings-table-row-info">
+                  <img src={booking.car.image} alt="car image" />
+                  <p>
+                    {booking.car.brand} {booking.car.model}
+                  </p>
+                </td>
+
+                {/* DATE RANGE */}
+                <td className="manage-bookings-table-row-dateRange">
+                  {booking.pickupDate.split("T")[0]} to{" "}
+                  {booking.returnDate.split("T")[0]}
+                </td>
+
+                {/* TOTAL */}
+                <td className="manage-bookings-table-row-total">
+                  {currency}
+                  {booking.price}
+                </td>
+
+                {/* PAYMENT */}
+                <td className="manage-bookings-table-row-payment">
+                  <span>offline</span>
+                </td>
+
+                {/* ACTIONS */}
+                <td className="manage-bookings-table-row-actions">
+                  {booking.status === "pending" ? (
+                    <select value={booking.status}>
+                      <option value="pending">Pending</option>
+                      <option value="cancelled">Cancelled</option>
+                      <option value="confirmed">Confirmed</option>
+                    </select>
+                  ) : (
+                    <span
+                      className={`${
+                        booking.status === "confirmed"
+                          ? "manage-bookings-confirmed"
+                          : "manage-bookings-cancelled"
+                      }`}
+                    >
+                      {booking.status}
+                    </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageBookings;
--- frontend/src/pages/Owner/ManageCars/ManageCars.jsx
+++ frontend/src/pages/Owner/ManageCars/ManageCars.jsx
@@ -1,7 +1,105 @@
+import { useEffect, useState } from "react";
+import Title from "../../../components/Owner/Title/Title.jsx";
+import { assets, dummyCarData } from "../../../assets/assets.js";
 import "./ManageCars.css";
 
 const ManageCars = () => {
-  return <div>ManageCars</div>;
+  const [cars, setCars] = useState([]);
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const fetchOwnerCars = async () => {
+    setCars(dummyCarData);
+  };
+
+  useEffect(() => {
+    fetchOwnerCars();
+  }, []);
+
+  return (
+    <div className="manage-cars">
+      <Title
+        title={"Manage Cars"}
+        subtitle={
+          "View all listed cars, update their details, or remove them from the booking platform."
+        }
+      />
+
+      {/* TABLE */}
+
+      <div className="manage-cars-table-container">
+        <table className="manage-cars-table">
+          <thead>
+            <tr className="manage-cars-table-header">
+              <th>Car</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {cars.map((car, index) => (
+              <tr key={index} className="manage-cars-table-row">
+                <td className="manage-cars-table-row-info">
+                  <img src={car.image} alt="car image" />
+
+                  {/* CAR INFO */}
+                  <div>
+                    <p>
+                      {car.brand} {car.model}
+                    </p>
+                    <p>
+                      {car.seating_capacity} • {car.transmission}
+                    </p>
+                  </div>
+                </td>
+
+                {/* CATEGORY */}
+                <td className="manage-cars-table-row-category">
+                  {car.category}
+                </td>
+
+                {/* PRICE PER DAY */}
+                <td className="manage-cars-table-row-price">
+                  {currency} {car.pricePerDay} /day
+                </td>
+
+                {/* STATUS */}
+                <td className="manage-cars-table-row-status">
+                  <span
+                    className={` ${
+                      car.isAvailable
+                        ? "manage-cars-available"
+                        : "manage-cars-unavailable"
+                    }`}
+                  >
+                    {car.isAvailable ? "Available" : "Unavailable"}
+                  </span>
+                </td>
+
+                {/* ACTIONS */}
+                <td className="manage-cars-table-row-actions">
+                  <img
+                    src={
+                      car.isAvailable ? assets.eye_close_icon : assets.eye_icon
+                    }
+                    alt="availability icon"
+                    className="manage-cars-table-row-actions-icon"
+                  />
+                  <img
+                    src={assets.delete_icon}
+                    alt="delete icon"
+                    className="manage-cars-table-row-actions-icon"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageCars;
